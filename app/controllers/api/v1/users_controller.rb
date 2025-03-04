d438677f53fdd9150c0798dd098d3b4ba8aa53ae
Stanislav Zaleski <sigmaray@gmail.com>
2025-03-04 19:24:09 +0300

    comment_count

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::UsersController < Api::V1::ApiController
   def index
     @users = User.all
-    render json: { users: @users }
+    # TODO: Decrease amount of SQL queries
+    users_json = @users.map do |user|
+      json = user.as_json
+      json[:comment_count] = user.comments.count
+      json
+    end
+    render json: { users: users_json }
   end
 end
