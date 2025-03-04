cf6c36d90328068589ebcc09e890bb2aad9b646b
Stanislav Zaleski <sigmaray@gmail.com>
2025-03-04 18:25:07 +0300

    rails g model Comment text:string user:references

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_04_151355) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_04_152408) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -25,6 +25,14 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_04_151355) do
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -36,4 +44,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_04_151355) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "comments", "users"
 end
